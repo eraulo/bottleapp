7af10b54cd1f4bd485cdf7f508fd3d7200ed1091
Mariano de Deus <dedeus_mariano@yahoo.com>
2024-10-07 14:31:21 +0900

    add app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+import os
 from bottle import Bottle, run
 
 app = Bottle()
 
 @app.route('/')
 def home():
-    return "Hello, Piku with Bottle!"
+    return "Hello, Piku!"
+
 
 if __name__ == "__main__":
-    run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    # Use the port provided by Piku
+    port = int(os.getenv('PORT', 8080))
+    run(app, host='0.0.0.0', port=port)
+
 
